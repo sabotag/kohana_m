b6379cb345c9b543474449bd345c33cf9aa7b8fb
Moroko Sergey <morokosv@gmail.com>
2013-03-26 23:45:19 +0200

    test

--- application/i18n/ru.php
+++ application/i18n/ru.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 return array(
-	'global.title' => 'mmatica',
+	'global.title' => 'mmatica ',
 	'global.sign_in' => 'Вход',
 	'global.logout' => 'Выход',
 	'global.registration' => 'Регистрация',
